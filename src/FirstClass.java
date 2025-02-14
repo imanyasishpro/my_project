d717e9a92cc01f67a87fe928956d7431f4273c68
Iman <imanyasishpro@gmail.com>
2025-02-14 13:02:21 +0530

    Tets

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,12 +1,31 @@
 public class FirstClass {
     public static void main(String[] arg){
 
-        int myMarks = 80;
-        int passMark = 75;
+        int myMarks = 40;
+        int aMark = 75;
+        int bMark = 65;
+        int cMark = 45;
+        int sMark = 35;
         int maxMark = 100;
 
-        if (myMarks > passMark && myMarks < maxMark) {
+        if (myMarks > aMark && myMarks < maxMark) {
             System.out.print("You got an A pass son but not the max mark.");
+        }
+
+        if (myMarks > bMark && myMarks < aMark) {
+            System.out.print("You got a B pass son");
+        }
+
+        if (myMarks > cMark && myMarks < bMark) {
+            System.out.print("You got a C pass son");
+        }
+
+        if (myMarks > sMark && myMarks < cMark) {
+            System.out.print("You got a S pass son");
+        }
+
+        if (myMarks < sMark) {
+            System.out.print("You are Fail son. Try next time");
         }
     }
 }
